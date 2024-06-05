a3a3b039a3bac55dd4cc6069f16d574674081e89
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-06-04 20:25:35 -0700

    add login functionality

---
+++ src/http/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+    // move this value to env variable 
+    baseURL: 'http://localhost:5513',
+    headers: {
+        "Content-Type": 'application/json'
+    }
+});
+
+// post method to send data 
+export const login = async (data: {email: string; password: string}) => api.post('/api/users/login', data); 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,12 +9,25 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { login } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      console.log("Login in successful ");
+
+      // redirect to dashboard
+      navigate("/dashboard/home");
+    },
+  });
 
   const handleLoginSubmit = () => {
     const email = emailRef.current?.value;
@@ -22,6 +35,13 @@ const LoginPage = () => {
 
     console.log("data", { email, password });
 
+    // if email or password is undeined
+    if (!email || !password) {
+      return alert("Enter email and password");
+    }
+
+    //mutation
+    mutation.mutate({ email, password });
     //make server call
   };
 
